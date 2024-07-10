0ba45da388498255a54274c43f7f866185a72f61
JicoHe <15089813315aa@gmail.com>
2024-07-10 13:05:34 +0800

    update

--- Study/chapter5/test.py
+++ Study/chapter5/test.py
@@ -1,2 +1,55 @@
 # 5-1 conditional tests
 # pass
+
+# 5-3 Alien Colors
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print('You just earned 5 points for shooting the alien.')
+elif 'yellow' in alien_color:
+    print("You just earned 10 points")
+elif 'red' in alien_color:
+    print("You just earned 15 points")
+
+# 5-6 Stages of Life
+age = 11
+
+if age < 2:
+    person = 'baby'
+elif age < 4:
+    person = 'toddler'
+elif age < 13:
+    person = 'kid'
+elif age < 20:
+    person = 'teenager'
+elif age < 65:
+    person = 'adult'
+elif age >= 65:
+    person = 'elder'
+print(f'the person is {person}')
+
+# 5-8
+username = ['admin', 'bob', 'tony', 'david', 'joji']
+for name in username:
+    if name == 'admin':
+        print('admin, welcome')
+    else:
+        print(f'{name} is not admin')
+for i in range(len(username)):
+    if username:
+        username.remove(username[0])
+print(len(username))
+
+# 5-10
+current_users = ['admin', 'bob', 'tony', 'david', 'joji']
+cap_users = current_users[:]
+# 让列表所有的名字大写
+for i in range(len(cap_users)):
+    if cap_users[i]:
+        cap_users[i] = cap_users[i].upper()
+new_users = ['DAVID', 'joji', 'don']  # 新用户名字
+# 确认是否可用
+for name in new_users:
+    if name not in current_users and name not in cap_users:
+        print(f'you can use {name} as your username ')
+    else:
+        print(f'{name} has been used')
--- Study/chapter5/topping.py
+++ Study/chapter5/topping.py
@@ -1,3 +1,37 @@
-requested_topping = 'mushroom'
-if requested_topping != 'anchovies':
+requested_toppings = ['mushroom']
+if requested_toppings != 'anchovies':
     print('Sorry, we don\'t have anchovies')
+
+requested_toppings.append('extra cheese')
+if 'mushroom' in requested_toppings:
+    print('Adding mushrooms.')
+if 'pepperoni' in requested_toppings:
+    print('Adding pepperoni.')
+if 'extra cheese' in requested_toppings:
+    print('Adding extra cheese.')
+
+requested_toppings.append('green peppers')
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print('Sorry, we don\'t have green peppers.')
+    else:
+        print('Adding ' + requested_topping)
+print("\nFinished making your pizza!")
+
+# 判断是否不是空的列表
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(requested_topping)
+else:
+    print('Sorry, we don\'t have any pizza.')
+
+# 列表与列表的比较
+available_toppings = ['mushroom', 'pepperoni', 'extra cheese', 'green peppers', 'Olives', 'Pineapples']
+requested_toppings = ['mushroom', 'extra cheese', 'green peppers']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(requested_topping)
+    else:
+        print('Sorry, we don\'t have ' + requested_topping)
+print("\nFinished making your pizza!")
